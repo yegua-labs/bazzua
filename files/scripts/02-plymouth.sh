e4e8bf7b1902b9bd1195210647dafa85ea01919d
Adam Fidel <adam@fidel.id>
2025-02-03 20:49:07 -0600

    feat: add plymouth theme

---
+++ files/scripts/02-plymouth.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+cd /usr/share/plymouth/themes
+curl -L https://github.com/adi1090x/plymouth-themes/releases/download/v1.0/rings.tar.gz | tar xvf -
+
+cat <<EOF >> rings/rings.script
+
+bazzite_image = Image("/usr/share/plymouth/themes/spinner/watermark.png");
+bazzite_sprite = Sprite();
+
+bazzite_sprite.SetImage(bazzite_image);
+bazzite_sprite.SetX(Window.GetX() + (Window.GetWidth() / 2 - bazzite_image.GetWidth() / 2)); # center the image horizontally
+bazzite_sprite.SetY(Window.GetHeight() - bazzite_image.GetHeight() - 50); # display just above the bottom of the screen
+EOF
+
+plymouth-set-default-theme rings
